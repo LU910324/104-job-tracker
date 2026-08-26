8aa65ba017f0da7b0228c4bde266062cd82dae0d
LU910324 <samlu97007@gmail.com>
2026-08-26 12:27:59 +0800

    Create uninstall-task.bat

---
+++ windows/uninstall-task.bat
@@ -0,0 +1,15 @@
+@echo off
+REM 移除之前安裝的 Windows 排程。
+REM 用法：滑鼠雙擊這個檔案就可以了。
+
+setlocal
+
+set "TASK_NAME=104-job-tracker"
+
+schtasks /delete /tn "%TASK_NAME%" /f
+
+echo.
+echo 已移除排程。
+pause
+
+endlocal
